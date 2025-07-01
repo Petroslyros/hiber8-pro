9f37527553b2517a47afd393f150d02a7c3eadab
Petros <petrosluros@gmail.com>
2025-07-01 20:43:11 +0300

    beans

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,10 +1,19 @@
 package gr.aueb.cf;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 /**
  * Hello world!
  */
 public class App {
+
+    private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school8PU");
+    private final static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
+
 
     }
 }
---
+++ src/main/java/gr/aueb/cf/model/Teacher.java
@@ -0,0 +1,61 @@
+package gr.aueb.cf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @Column(name = "firstname", length = 255, nullable = true,unique = false)
+    private String firstname;
+
+    //    @Column(name = "firstname", length = 255, nullable = true,unique = false)
+    private String lastname;
+
+    public Teacher(){
+
+    }
+
+    public Teacher(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
